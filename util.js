6eba994c8d810984091deb496fc04dabf43d195d
OgbajeLeo <leonardaromeh@gmail.com>
2023-11-02 09:15:57 +0100

    UI improved

--- util.js
+++ util.js
@@ -8,20 +8,28 @@ function uuid() {
 }
 
 //Database
-const todo_DB =() =>{ JSON.parse(localStorage.getItem(DB_NAME) )|| []}
+const todo_DB =() =>{ return (JSON.parse(localStorage.getItem(DB_NAME) )|| [])}
+
+const saveToDatabase=(key,value)=>{return (localStorage.setItem(key, JSON.stringify(value)))}
 
 
-//InputPrompt
-const inputCheck =(toast)=>{
-    if(!todoInput.value){
+//convert to Date format
+const todoDate= (todoD) => {
+const currentDate = new Date(todoD);
 
-        const formMessage = document.querySelector("#prompt");       
-        formMessage.classList.remove('hidden');
-        formMessage.innerHTML = toast
+const year = currentDate.getFullYear();
+const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+const day = currentDate.getDate().toString().padStart(2, '0');
+const hours = currentDate.getHours().toString().padStart(2, '0');
+const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+let amOrPm = 'AM';
 
-        setTimeout(()=>{
-            formMessage.classList.add('hidden')
-        },5000)
-        return
-    }
+// Convert hours to 12-hour format and set AM or PM
+if (hours >= 12) {
+  amOrPm = 'PM';
+  hours = (hours % 12) || 12; // If hours is 0, set it to 12
+}
+
+const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}${amOrPm}`
+return (formattedDate)
 }
