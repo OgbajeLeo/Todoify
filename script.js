be3df4a4e1e14c022bd2f8b21d85321c41a33c2e
OgbajeLeo <leonardaromeh@gmail.com>
2023-11-03 02:26:19 +0100

    UI improved and Preview Page Added

--- script.js
+++ script.js
@@ -45,13 +45,12 @@ const fetchTodo = () => {
   });
 
   const mappedTodos = todos.map((todo) => [
-   //todoDate(todo.created_on)
 
-    ` <div class="group flex items-center justify-between p-4 mb-4 rounded-md w-full h-16 bg-slate-400 hover:bg-slate-700 text-slate-100 shadow-md">
-            <a href="" class="font-bold text-black hover:text-white">${todo.title}<br><span class="text-[9px] text-gray-400">${todoDate(todo.created_on)}</span></a> 
+    ` <div class="group flex items-center justify-between p-4 mb-4 rounded-md w-full h-16 bg-slate-100 hover:bg-slate-200 text-slate-100 shadow-md">
+            <button onclick="todoToProview('${todo.uuid}')" class="font-bold text-black truncate" style='max-width:400px;'>${todo.title}</button> 
             <section class="flex hidden gap-6 group-hover:block  pl-8">
                 <button onclick="handleEditMode('${todo.uuid}')" title="Edit">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="cyan" class="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />   
                 </svg>
                 </button>
@@ -142,4 +141,13 @@ const updateTodo = (id) => {
   addBtn.classList.remove("hidden");
   updateBtn.classList.add("hidden");
 };
+
+
+const todoToProview =(id)=>{
+saveToDatabase('todoToPreview',id)
+
+window.location.href='/preview-todo.html'
+}
+
+
 fetchTodo();
--- util.js
+++ util.js
@@ -14,13 +14,13 @@ const saveToDatabase=(key,value)=>{return (localStorage.setItem(key, JSON.string
 
 
 //convert to Date format
-const todoDate= (todoD) => {
+const todoDate = (todoD) => {
 const currentDate = new Date(todoD);
 
 const year = currentDate.getFullYear();
 const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
 const day = currentDate.getDate().toString().padStart(2, '0');
-const hours = currentDate.getHours().toString().padStart(2, '0');
+let hours = currentDate.getHours().toString().padStart(2, '0');
 const minutes = currentDate.getMinutes().toString().padStart(2, '0');
 let amOrPm = 'AM';
 
@@ -30,6 +30,6 @@ if (hours >= 12) {
   hours = (hours % 12) || 12; // If hours is 0, set it to 12
 }
 
-const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}${amOrPm}`
+const formattedDate = `${year}/${month}/${day} ${hours}:${minutes}${amOrPm}`
 return (formattedDate)
 }
